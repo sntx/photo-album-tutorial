55fa3aafcd305c6c3a2994279a4d70332c0ef353
lucidolab <santiago.lucidolab@gmail.com>
2020-06-25 10:19:02 +0200

    get images

--- App.tsx
+++ App.tsx
@@ -1,19 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, View, Image } from "react-native";
+import Axios from "axios";
+
+const GET_GALAXY_IMAGES =
+  "https://images-api.nasa.gov/search?q=spiral%20galaxies&media_type=image";
 
 export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  // Here we are calling Axios.get() inside React's useEffect hook. We need to
+  // get our async data like this so our component can properly update it's
+  // state.
+  //
+  // For useEffect() to be called only once, we add an empty array as the second
+  // argument of useEffect()
+  //
+  // More on useEffect(): https://reactjs.org/docs/hooks-effect.html
+  //
+  useEffect(() => {
+    Axios.get(GET_GALAXY_IMAGES)
+      .then(({ data }) => {
+        console.log("data.collection.itmes", data.collection.items);
+        setData(data.collection.items);
+      })
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  // After loading is done "isLoading", we render a FlatList with the data that
+  // was set on the success axios callback above "setData(...)"
+  //
+  // Finally we render each of our images inside FlatList's renderImage prop
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={(_item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <Image
+              style={{ height: 100, width: 100 }}
+              source={{ uri: item.links[0].href }}
+            />
+          )}
+        />
+      )}
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
